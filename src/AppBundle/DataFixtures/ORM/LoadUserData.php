f99a7233db8959befbc6557290ccaddd48ec0221
Kamil Breguła <mik-laj@users.noreply.github.com>
2016-08-08 12:31:41 +0200

    Authorisation (#20)
    * First step configure FOSUserBundle
    
    * Fix template of form
    
    * Add users fixture
    
    * Fix login/logout link
    
    * Style CI

---
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,48 @@
+<?php
+namespace AppBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\User;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class LoadUserData implements FixtureInterface, ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+
+        $userManager = $this->container->get('fos_user.user_manager');
+        $credentials = [
+            [
+                'login'     => 'root', 
+                'password'  => 'root', 
+                'email'     => 'root@example.com',
+            ],
+            [
+                'login'     => 'michal', 
+                'password'  => 'kokoszka', 
+                'email'     => 'michal@example.com',
+            ],
+        ];
+
+        foreach ($credentials as $credential) {
+            $user = new User();
+            $user->setUsername($credential['login']);
+            $user->setEmail($credential['email']);
+            $user->setPlainPassword($credential['password']);
+            $user->setEnabled(true);
+            $userManager->updateUser($user, true);
+        }
+    }
+}
