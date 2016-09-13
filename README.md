0c7022db40f6aec870b1db65b2e87c00c7b23e45
mik-laj <kamil.bregula@siecobywatelska.pl>
2016-09-13 16:44:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+bip.siecobywatelska.pl
+======================
+
+[![StyleCI](https://styleci.io/repos/63485799/shield)](https://styleci.io/repos/63485799)
---
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\User;
+
+class LoadUserData implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $userAdmin = new User();
+        $userAdmin->setUsername('root');
+        $userAdmin->setPassword('root');
+        $userAdmin->setEmail('root@example.org');
+        $userAdmin->isEnabled(true);
+        $userAdmin->setRoles(array('ROLE_SUPER_ADMIN'));
+
+        $manager->persist($userAdmin);
+        $manager->flush();
+    }
+}
