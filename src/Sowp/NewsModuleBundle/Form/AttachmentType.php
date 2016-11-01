e619d1a9acb6a4e25f24ac349980d9c37950e36d
jakowaty <herbalist@herbalist.hekko.pl>
2016-11-01 20:10:13 +0100

    layouts, AttachmentType

---
+++ src/Sowp/NewsModuleBundle/Form/AttachmentType.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Sowp\NewsModuleBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AttachmentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name', TextType::class);
+        $builder->add('file', FileType::class, ['data_class' => null]);
+    }
+}
