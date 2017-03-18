ae20a0d8fe7dc4f208e609bb7bb48c089f07d4b4
jakowaty <herbalist@herbalist.hekko.pl>
2017-03-18 18:51:28 +0100

    adding api bundle

---
+++ src/Sowp/ApiBundle/ApiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Sowp\ApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ApiBundle extends Bundle
+{
+}
---
+++ src/Sowp/ApiBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Sowp\ApiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('ApiBundle:Default:index.html.twig');
+    }
+}
