afb7f5e2efef618b5116fcef8daf3db5a81a912d
jakowaty <herbalist@herbalist.hekko.pl>
2017-05-14 11:34:58 +0200

    move attachment type to UploadBundle

---
+++ src/Sowp/UploadBundle/Form/AttachmentType.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Sowp\UploadBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class AttachmentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name', TextType::class);
+        $builder->add('file', FileType::class, array('data_class' => null));
+    }
+}
